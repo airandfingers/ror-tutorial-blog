d4bc9f4e0e458a151cfed6c4ce32f634e4e8639c
Aaron Y <aarony@Aarons-MacBook-Pro.local>
2024-10-23 00:58:25 -0600

    read AUTH_USER and AUTH_SECRET from /etc/secrets instead of environment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: ENV['AUTH_USER'], password: ENV['AUTH_SECRET'], except: [:index, :show]
+  AUTH_USER = File.read('/etc/secrets/AUTH_USER').strip
+  AUTH_SECRET = File.read('/etc/secrets/AUTH_SECRET').strip
+
+  http_basic_authenticate_with name: AUTH_USER, password: AUTH_SECRET, except: [:index, :show]
 
   def index
     @articles = Article.all
